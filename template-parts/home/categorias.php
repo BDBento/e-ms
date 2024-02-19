3bdfe464b49590b6eceef1f8183afdb332f84308
BDBento <bdegan2016@gmail.com>
2024-02-19 16:00:54 -0400

    .

--- template-parts/home/categorias.php
+++ template-parts/home/categorias.php
@@ -5,95 +5,95 @@
                 <p>Encontre o sistema deseja navegando pelas categorias abaixo.</p>
             </div>
             <div class="btn-categotias">
-                <div class="categoria_grid">
-                    <div class="categoria_content">
+                <div class="categoria_grid row">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Administração.svg" alt=""><p>Administração Pública</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Administração.svg" alt=""><p class="col-9">Administração Pública</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Agricultura.svg" alt=""><p>Agricultura e Pecuária</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Agricultura.svg" alt=""><p class="col-9">Agricultura e Pecuária</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/suporte.svg" alt=""><p>Atendimento de suporte</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/suporte.svg" alt=""><p class="col-9">Atendimento de suporte</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Cidadania.svg" alt=""><p>Cidadania e Previdência</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Cidadania.svg" alt=""><p class="col-9">Cidadania e Previdência</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Ciência.svg" alt=""><p>Ciência e Tecnologia</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Ciência.svg" alt=""><p class="col-9">Ciência e Tecnologia</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Cultura.svg" alt=""><p>Cultura, Arte, História e Esportes</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Cultura.svg" alt=""><p class="col-9">Cultura, Arte, História e Esportes</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Educação.svg" alt=""><p>Educação</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Educação.svg" alt=""><p class="col-9">Educação</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Emprego.svg" alt=""><p>Emprego e Trabalho</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Emprego.svg" alt=""><p class="col-9">Emprego e Trabalho</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Energia.svg" alt=""><p>Energia</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Energia.svg" alt=""><p class="col-9">Energia</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Indústria.svg" alt=""><p>Indústria e Comércio</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Indústria.svg" alt=""><p class="col-9">Indústria e Comércio</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Meio_Ambiente.svg" alt=""><p>Meio Ambiente</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Meio_Ambiente.svg" alt=""><p class="col-9">Meio Ambiente</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Ônibus.svg" alt=""><p>Ônibus</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Ônibus.svg" alt=""><p class="col-9">Ônibus</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pagamento_de_Taxas.svg" alt=""><p>Pagamento de Taxas</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Pagamento_de_Taxas.svg" alt=""><p class="col-9">Pagamento de Taxas</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Saneamento.svg" alt=""><p>Saneamento</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Saneamento.svg" alt=""><p class="col-9">Saneamento</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Saúde.svg" alt=""><p>Saúde e Bem - estar</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Saúde.svg" alt=""><p class="col-9">Saúde e Bem - estar</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Segurança.svg" alt=""><p>Segurança e Justiça</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Segurança.svg" alt=""><p class="col-9">Segurança e Justiça</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Supervisor.svg" alt=""><p>Supervisor</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Supervisor.svg" alt=""><p class="col-9">Supervisor</p></button>
                         </a>
                     </div>
-                    <div class="categoria_content">
+                    <div class="categoria_content col-3">
                         <a href="#">
-                            <button class="categoria-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/Transporte_e_Trânsito.svg" alt=""><p>Transporte e Trânsito</p></button>
+                            <button class="categoria-item row"><img class="col-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/Transporte_e_Trânsito.svg" alt=""><p class="col-9">Transporte e Trânsito</p></button>
                         </a>
                     </div>
                 </div>
